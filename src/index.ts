c1fa1019aaa539ec7574f836c23284a132c596fd
Anas Mazouni <madadj4@gmail.com>
2020-07-21 02:15:49 +0200

    fix: disable caching

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ const app = express();
 const port = process.env.PORT || 6969;
 const path = 'media';
 
+app.set('etag', false);
+
 app.get('/', (req, res) => {
   res.send('Haha meme go brrr.');
 });
@@ -15,6 +17,7 @@ app.get('/send/memes', async (req, res) => {
     const memes = await fsPromises.readdir(path);
     if (memes.length > 0) {
       const randomMeme = memes[Math.floor(Math.random() * memes.length)];
+      res.set('Cache-Control', 'no-store');
       res.sendFile(randomMeme, {
         root: `${__dirname}/../media/`,
       });
